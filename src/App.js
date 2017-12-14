bca44211b089aa9cc12752958a9c005d61a9ecbf
kirillzykov <kirillzykov777@gmail.com>
2017-12-15 01:15:50 +0300

    ssp 5-6 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-/*import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          1 get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;*/
 import React, { Component } from 'react';
 import './App.css';
 import UserInfo from './Components/UserInfo.js';
